962cab6353866189da7681271a6ceacbc5567cf7
Shawn Wilkinson <super3boy@gmail.com>
2012-09-09 23:34:15 -0400

    Service Page Done

--- services.php
+++ services.php
@@ -2,7 +2,221 @@
 <div class="title">
 	<h1>Services</h1>
 	<div class="sub">
-		Hello World!
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_16.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>PERSONAL BRAND DEVELOPMENT</h3>
+				Personal branding is the process of differentiating oneself from others within designated professions or spaces of recognition.  It is the conscious decisions of 
+				individuals to choose and/or highlight distinguishing qualities for interacting with others. Branding certification agency Reach 360 describes personal branding as 
+				the new revolution in the way professionals manage their careers or businesses, a way of clarifying and communicating what makes them unique and using those qualities
+				to separate themselves from peers as a means of greatly expanding success. 
+				<br/><br/>
+				Similarly, Wikipedia defines personal branding as the process whereby people and their careers are marked as brands. It has been noted that while previous self-help 
+				management techniques were about self-improvement, the personal branding concept suggests that success comes from self-packaging. Personal branding is the strategy 
+				behind the world's most successful people. It is often the difference between an ordinary career or business and an exceptional one.
+				<br/><br/>
+				Clients that decide to develop personal brands will initially complete an online assessment that will be used to develop the documentation that will define and support
+				personal brand development. 
+			</div>
+			<div class="span10 spacer"></div>
+		</div>
+
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_13.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>ASSESSMENT TOOLS</h3>
+				Personal Consultation, Reach 360 and StrengthFinders are the primary tools used to assess and develop personal brands for clients.  A brief description of each tool is listed below.  
+				<br/><br/>
+				<ul>
+					<li>
+						Personal Consultation
+						<ul>
+							<li>One-hour session used to discuss Pro Rx services and better understand the clients’ personal and professional goals. The information gathered in this session will serve as the foundation 
+								for positioning statement and Brand strategy development.</li>
+						</ul>
+					</li>
+					<li>
+						Reach 360 Personal Branding
+						<ul>
+							<li>The leading personal branding assessment. Your personal brand is held in the hearts and minds of those around you. What would others say are your greatest strengths and weaknesses?
+								Use our personal branding assessment to understand what people think about you.</li>
+						</ul>
+					</li>
+					<li>
+						StrengthFinder Assessment
+						<ul>
+							<li>StrengthsFinder is an assessment tool to that helps professionals discover and describe their talents. This assessment focuses on the positive while accentuating what’s RIGHT as opposed to 
+								what’s wrong. Focusing on the positive helps clients to articulate their interests and high performance areas.</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="span10 spacer"></div>
+		</div>
+
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_06.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>PROFESSIONAL DOCUMENT REVIEW</h3>
+				Choosing the proper documentation to communicate candidate qualification information is essential to effective evaluation by potential employers.  The Resume, Curriculum Vitae (CV)
+				and cover letter are the most widely accepted tools for communicating candidate information. The cover letter is used to draw attention to specified qualifications of the candidate
+				and introduce the resume or CV.  The decision to use a resume (and resume type) versus a CV largely depends upon the industry in which the candidate is seeking the opportunity.  
+				<br/><br/>
+				Resumes are the most commonly accepted tool among job seekers.  They are recognized across most industries and generally developed in one of the four formats (chronological, functional, 
+				combination and targeted) listed below.  
+				<br/><br/>
+				The Curriculum Vitae (CV) is a longer, more detailed form of resume most commonly used in the academic, medical, legal, educational and science fields to outline experience and education.
+				CV’s also list other relevant items e.g. publications, certifications, courses taught, presentations, awards and honors and professional associations of potential candidates. 
+			</div>
+			<div class="span10 spacer"></div>
+		</div>
+
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_03.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>INTERACTIVE COMMUNICATION PREPARATION</h3>
+				<ul>
+					<li>Interactive Preparation</li>
+					<ul>
+						<li>Phone Screen – Assisting clients in preparation for phone screens with prospective employers, agencies and institutions by explaining the purpose, goals and objectives of the call.</li>
+						<li>Individual Personal Interview – Readying clients for individual interviews by developing and employing strategic branded techniques for behavioral interviews.</li>
+						<li>Series of Interviews – Working with clients to develop a progressive branded strategy for consistently executing a series of cohesive interviews with a prospective employer, agency or institution.</li>
+						<li>Group Interview – Partnering with clients to master the art of effectively communication branded information during a single interview with multiple employment agency or institutional representatives.</li> 
+					</ul>
+					<li>Follow-Up Methodology</li>
+					<ul>
+						<li>Phone Call – Interaction to ensure that clients understand the timing and strategy of talking with prospective profession suitors via phone.</li>
+						<li>Email – Explaining the technique for timely branded communication via email using personal brand attributes.</li>
+						<li>Card – Helping clients to select and/or design a card that communicates the brand attributes identified in the personal assessment.</li>
+						<li>Letter – Assisting clients with development and timing of follow-up and thank letters that include the personal brand attributes identified in the initial assessment.</li>
+					</ul>
+				</ul>
+			</div>
+			<div class="span10 spacer"></div>
+		</div>
+
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_08.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>SALARY AND PERFORMANCE NEGOTIATION TRAINING</h3>
+				<ul>
+					<li>Salary Offer Negotiation and Counteroffer Proposal Components</li>
+					<ul>
+						<li>What makes an offer truly an offer? - Explaining salary offer components</li>
+						<li>First rule of negotiating - Overview of how to start the negotiation process</li>
+						<li>Negotiating in Good Faith - Defining a “Good Faith” Offer and Response</li>
+						<li>Developing a counteroffer - 7-Steps for developing branded counteroffer</li>
+						<li>Closing The Deal - Instructional steps for finalizing an employment offer</li>
+					</ul>
+				</ul>
+			</div>
+			<div class="span10 spacer"></div>
+		</div>
+
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_02.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>SUCCESSION PLANNING</h3>
+				<ul>
+					<li>Performance Review Preparation and Response Tools</li>
+					<ul>
+						<li>Develop job responsibility matrix (typical week) - Worksheet</li>
+						<li>Review job performance metrics - Outlining the key review components</li>
+						<li>Develop an action plan and setting the performance review meeting strategy</li>
+						<li>Setting future review expectations</li>
+						<li>Closing the deal</li>
+					</ul>
+				</ul>
+			</div>
+			<div class="span10 spacer"></div>
+		</div>
+
+		<div>
+			<div class="span2">
+				<img src="images/icons/handy-icon_01.png" alt="icon"/>
+			</div>
+			<div class="span8">
+				<h3>PROFESSIONAL MENTORING AND COACHING</h3>
+				Pro Rx Professional Mentoring is the process of providing proficient knowledge and practical expertise about a given field as well as answers to job execution questions.
+				The Pro Rx methodology uses an initial consultation to assess client needs, research industry and company information to develop branded action plans that highlight strengths. 
+				Once a mentoring package is agreed upon, SMART Goals are defined, an action plan is outlined and regular review meetings are scheduled to assess progress, review findings and amend 
+				the action plan. Regularly scheduling meetings, as described in the action plan, will ensure alignment and assist all parties in achieving the desired goals. In these meetings, 
+				clients will have the opportunity to discuss and/or role play a range of ideas and methods before deciding a course of action. 
+				<br/><br/>
+				Many professionals wish to focus development on gaining wealth and professional recognition. Achieving these goals usually involved satisfying a number of requirements laid down by
+				your employer or institution. Obtaining help and guidance from a senior colleague who is knowledgeable about the requirements and the level of achievement expected is generally
+				helpful however; the professionals seeking guidance and insight should beware that a personal bias may exist and the risk of internal exposure within the organization is a real
+				threat to success. 
+				<br/><br/>
+				At Pro Rx, Coaching is a 360-degree, 1:1 developmental process. There are many methods of executing professional coaching. Traditionally, the most common arrangement is for a private,
+				one-on-one, 30-45 minute coaching sessions that take place via scheduled web-based meetings or telephone sessions as defined by the clients needs. Many clients choose to meet weekly 
+				while others secure Pro Rx services two or three times per month. Many opt for face-to-face meetings instead of web-based or telephone sessions. The more frequent the sessions, the easier
+				it is to keep the process moving forward.
+				<br/><br/>
+				During these sessions, your Pro Rx representative will:
+
+				<ul>
+					<li>Ask questions to make you think</li>
+					<li>Help you set SMART Goals</li>
+					<li>Provide clarity and focus on goals, aspirations and ambitions</li>
+					<li>Challenge you to go for the gold</li>
+					<li>Welcome and reinforce success</li>
+				</ul>
+
+				The objectives  of the coaching sessions are to enhance professional development and provide guidance towards obtaining the desired personal and professional goals.  The areas of focus are:
+
+				<ul>
+					<li>Situation Analysis</li>
+					<ul>
+						<li>Strength Assessment</li>
+						<li>Personal Actualization Sessions</li>
+					</ul>
+					<li>Goal Setting</li>
+					<ul>
+						<li>Desired Outcome</li>
+						<li>Timeline</li>
+					</ul>
+					<li>Execution Plan Development</li>
+					<ul>
+						<li>Scheduled meetings</li>
+						<li>Action Items</li>
+						<li>Next Steps</li>
+					</ul>
+				</ul>
+			</div>
+			<div class="span10 spacer"></div>
+	</div>
+
+	<div>
+		<div class="span2">
+			<img src="images/icons/handy-icon_07.png" alt="icon"/>
+		</div>
+		<div class="span8">
+			<h3>CRISIS MANAGEMENT</h3>
+			Crisis Management is the execution path selected in an emergency situation. This path should mitigate future damages and position the client to respond to damages that are present or
+			unfolding.  Professionals in crisis generally seek to address the impending challenge and assure stakeholders that the situation is being managed. Pro Rx manages provides four stages of 
+			training and advice for crisis management. The stages are:
+			<ul>
+				<li>Crisis Prevention</li>
+				<li>Crisis De-escalation</li>
+				<li>Crisis Intervention</li>
+				<li>Post-Crisis Intervention</li>
+			</ul>
+		</div>
+	</div>
+
 	</div>
 </div>
 <?php include_once('footer.php'); ?>
